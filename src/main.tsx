1d5ff694eba8810d68d22be3e0c7b16f3a2dc63b
catOWO2011 <catOWO2011@gmail.com>
2023-11-16 14:56:53 -0400

    Setup initial

---
+++ src/main.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import 'semantic-ui-css/semantic.min.css'
+
+import App from './app/layout/App'
+import './app/layout/styles.css'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
